35bf57275dd27c80eb3c3e4dc23f270ec13cf9fc
Neha <neharaao2022@gmail.com>
2024-01-04 16:30:24 +0530

    Use css in timeline project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Time from "./Timeline"
+import './App.css'
 
 const App =()=>{
     return(
-        <div>
-            <h1>TimeLine </h1>
+        <div className="container">
+            <h1 className="Head">TimeLine </h1>
             <Time year = "2004" event = "This year is my born "/>
             <Time year = "2008" event = "This year I am go to Brat"/>
             <Time year = "2012" event = "This is I am go to Hostel"/>
